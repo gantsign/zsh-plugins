b5a7c9302252e22789cc9a528b833ce616b5a21b
John Freeman <john.freeman.opensource@gmail.com>
2022-12-04 10:41:43 +0000

    Fixed write to cache dir if not exists
    Needed to create the `completions` directory.

--- kubeadm/kubeadm.plugin.zsh
+++ kubeadm/kubeadm.plugin.zsh
@@ -3,7 +3,7 @@ if (( $+commands[kubeadm] )); then
     # If the completion file does not exist, generate it and then source it
     # Otherwise, source it and regenerate in the background
     if [[ ! -f "$ZSH_CACHE_DIR/completions/_kubeadm" ]]; then
-        mkdir -p "$ZSH_CACHE_DIR"
+        mkdir -p "$ZSH_CACHE_DIR/completions"
         kubeadm completion zsh | tee "$ZSH_CACHE_DIR/completions/_kubeadm" >/dev/null
         source "$ZSH_CACHE_DIR/completions/_kubeadm"
     else
