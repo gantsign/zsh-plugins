2cca1c317180c86d3a296d879fe012fb39d5a063
John Freeman <john.freeman.opensource@gmail.com>
2022-12-04 09:40:06 +0000

    Added completion caching
    For improved performance.

--- kompose/kompose.plugin.zsh
+++ kompose/kompose.plugin.zsh
@@ -1,3 +1,13 @@
-if [ $commands[kompose] ]; then
-    eval "$(kompose completion zsh)"
+if (( $+commands[kompose] )); then
+    ZSH_CACHE_DIR="${ZSH_CACHE_DIR:-$HOME/.cache/zsh}"
+    # If the completion file does not exist, generate it and then source it
+    # Otherwise, source it and regenerate in the background
+    if [[ ! -f "$ZSH_CACHE_DIR/completions/_kompose" ]]; then
+        mkdir -p "$ZSH_CACHE_DIR"
+        kompose completion zsh | tee "$ZSH_CACHE_DIR/completions/_kompose" >/dev/null
+        source "$ZSH_CACHE_DIR/completions/_kompose"
+    else
+        source "$ZSH_CACHE_DIR/completions/_kompose"
+        kompose completion zsh | tee "$ZSH_CACHE_DIR/completions/_kompose" >/dev/null &|
+    fi
 fi
