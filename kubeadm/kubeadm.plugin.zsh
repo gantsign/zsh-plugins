9f18fa6fd04f3a9f38c647cb900d7533938125d1
John Freeman <john.freeman.opensource@gmail.com>
2022-12-04 09:41:26 +0000

    Added completion caching (#22)
    For improved performance.

--- kubeadm/kubeadm.plugin.zsh
+++ kubeadm/kubeadm.plugin.zsh
@@ -1,3 +1,13 @@
-if [ $commands[kubeadm] ]; then
-    eval "$(kubeadm completion zsh)"
+if (( $+commands[kubeadm] )); then
+    ZSH_CACHE_DIR="${ZSH_CACHE_DIR:-$HOME/.cache/zsh}"
+    # If the completion file does not exist, generate it and then source it
+    # Otherwise, source it and regenerate in the background
+    if [[ ! -f "$ZSH_CACHE_DIR/completions/_kubeadm" ]]; then
+        mkdir -p "$ZSH_CACHE_DIR"
+        kubeadm completion zsh | tee "$ZSH_CACHE_DIR/completions/_kubeadm" >/dev/null
+        source "$ZSH_CACHE_DIR/completions/_kubeadm"
+    else
+        source "$ZSH_CACHE_DIR/completions/_kubeadm"
+        kubeadm completion zsh | tee "$ZSH_CACHE_DIR/completions/_kubeadm" >/dev/null &|
+    fi
 fi
--- minikube-none/minikube.plugin.zsh
+++ minikube-none/minikube.plugin.zsh
@@ -1,73 +1,78 @@
-__minikube_none() {
-    local -a pre_args=()
-    local -a post_args=()
-    local has_vm_driver='false'
-    local command=''
-    local key
+if (( $+commands[minikube] )); then
+    minikube() {
+        local -a pre_args=()
+        local -a post_args=()
+        local has_vm_driver='false'
+        local command=''
+        local key
 
-    while [[ $# -gt 0 ]]; do
-        if [[ "$command" == '' ]]; then
-            case "$1" in
-                --vm-driver*)
-                    has_vm_driver='true'
-                    pre_args+=("$1")
-                    shift
-                ;;
-                -b|--bootstrapper|--log_backtrace_at|--log_dir|--loglevel|-p|--profile|--stderrthreshold|-v|--v|--vmodule)
-                    pre_args+=("$1")
-                    shift
-                    pre_args+=("$1")
-                    shift
-                ;;
-                -*)
-                    pre_args+=("$1")
-                    shift
-                ;;
-                *)
-                    command="$1"
-                    shift
-                ;;
-            esac
+        while [[ $# -gt 0 ]]; do
+            if [[ "$command" == '' ]]; then
+                case "$1" in
+                    --vm-driver*)
+                        has_vm_driver='true'
+                        pre_args+=("$1")
+                        shift
+                    ;;
+                    -b|--bootstrapper|--log_backtrace_at|--log_dir|--loglevel|-p|--profile|--stderrthreshold|-v|--v|--vmodule)
+                        pre_args+=("$1")
+                        shift
+                        pre_args+=("$1")
+                        shift
+                    ;;
+                    -*)
+                        pre_args+=("$1")
+                        shift
+                    ;;
+                    *)
+                        command="$1"
+                        shift
+                    ;;
+                esac
+            else
+                case "$1" in
+                    --vm-driver*)
+                        has_vm_driver='true'
+                        post_args+=("$1")
+                        shift
+                    ;;
+                    *)
+                        post_args+=("$1")
+                        shift
+                    ;;
+                esac
+            fi
+        done
+
+        if [[ "$command" == 'start' ]] && [[ "$has_vm_driver" == 'false' ]]; then
+            export MINIKUBE_HOME=$HOME
+            export CHANGE_MINIKUBE_NONE_USER=true
+
+            mkdir -p $HOME/.kube
+
+            touch $HOME/.kube/config
+            export KUBECONFIG=$HOME/.kube/config
+
+            if [ -f /run/systemd/resolve/resolv.conf ]; then
+                post_args=('--extra-config=kubelet.resolv-conf=/run/systemd/resolve/resolv.conf' "${post_args[@]}")
+            fi
+
+            (set -x ; sudo --preserve-env ${commands[minikube]} --vm-driver=none \
+                "${pre_args[@]}" "$command" "${post_args[@]}")
         else
-            case "$1" in
-                --vm-driver*)
-                    has_vm_driver='true'
-                    post_args+=("$1")
-                    shift
-                ;;
-                *)
-                    post_args+=("$1")
-                    shift
-                ;;
-            esac
+            ${commands[minikube]} "${pre_args[@]}" "$command" "${post_args[@]}"
         fi
-    done
+    }
 
-    if [[ "$command" == 'start' ]] && [[ "$has_vm_driver" == 'false' ]]; then
-        export MINIKUBE_HOME=$HOME
-        export CHANGE_MINIKUBE_NONE_USER=true
-
-        mkdir -p $HOME/.kube
-
-        touch $HOME/.kube/config
-        export KUBECONFIG=$HOME/.kube/config
-
-        if [ -f /run/systemd/resolve/resolv.conf ]; then
-            post_args=('--extra-config=kubelet.resolv-conf=/run/systemd/resolve/resolv.conf' "${post_args[@]}")
-        fi
-
-        (set -x ; sudo --preserve-env minikube --vm-driver=none \
-            "${pre_args[@]}" "$command" "${post_args[@]}")
+    ZSH_CACHE_DIR="${ZSH_CACHE_DIR:-$HOME/.cache/zsh}"
+    # If the completion file does not exist, generate it and then source it
+    # Otherwise, source it and regenerate in the background
+    if [[ ! -f "$ZSH_CACHE_DIR/completions/_minikube" ]]; then
+        mkdir -p "$ZSH_CACHE_DIR"
+        minikube completion zsh | tee "$ZSH_CACHE_DIR/completions/_minikube" >/dev/null
+        source "$ZSH_CACHE_DIR/completions/_minikube"
     else
-        minikube "${pre_args[@]}" "$command" "${post_args[@]}"
+        source "$ZSH_CACHE_DIR/completions/_minikube"
+        minikube completion zsh | tee "$ZSH_CACHE_DIR/completions/_minikube" >/dev/null &|
     fi
-}
-
-if [[ $commands[minikube] ]]; then
-    source <(minikube completion zsh)
-
-    alias minikube=__minikube_none
-
-    # So tab-completion still works
-    compdef __minikube_none='minikube'
 fi
